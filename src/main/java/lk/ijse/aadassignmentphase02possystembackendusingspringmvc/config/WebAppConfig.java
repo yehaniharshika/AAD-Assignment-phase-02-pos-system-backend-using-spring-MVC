deb9cabd6246b99b9b1462222f83b2adb4d0841b
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-10-12 11:29:52 +0530

    Create WebAppConfig

---
+++ src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/config/WebAppConfig.java
@@ -0,0 +1,18 @@
+package lk.ijse.aadassignmentphase02possystembackendusingspringmvc.config;
+
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+//root package eka - lk.ijse.introspringmvc
+@ComponentScan(basePackages = "lk.ijse.aadassignmentphase02possystembackendusingspringmvc")
+@EnableWebMvc
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2,
+        maxFileSize = 1024 * 1024 * 5,
+        maxRequestSize = 1024 * 1024 * 10
+)
+public class WebAppConfig {
+}
