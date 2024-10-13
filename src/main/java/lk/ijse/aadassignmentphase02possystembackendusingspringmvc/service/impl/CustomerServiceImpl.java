7439d238aaabc9e0fc4f5d1a18f0cf40bc514da9
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-10-13 12:25:42 +0530

    customer save part complete

--- src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/service/impl/CustomerServiceImpl.java
@@ -1,15 +1,31 @@
 package lk.ijse.aadassignmentphase02possystembackendusingspringmvc.service.impl;
 
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dao.CustomerDAO;
 import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dto.CustomerStatus;
 import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dto.impl.CustomerDTO;
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.entity.CustomerEntity;
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.exception.DataPersistException;
 import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.service.CustomerService;
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Autowired
+    private Mapping customerMapping;
     @Override
     public void saveCustomer(CustomerDTO customerDTO) {
-        
+        CustomerEntity savedCustomer = customerDAO.save(customerMapping.toCustomerEntity(customerDTO));
+        if (savedCustomer == null){
+            throw new DataPersistException("Customer not saved");
+        }
     }
 
     @Override
