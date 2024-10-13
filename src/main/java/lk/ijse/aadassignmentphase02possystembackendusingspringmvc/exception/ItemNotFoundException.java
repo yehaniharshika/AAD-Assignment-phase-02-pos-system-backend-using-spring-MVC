897cfe571ad1c8e568f6fce71d55ed0f4ce820f6
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-10-14 00:30:15 +0530

    implement logics ItemNotFoundException custom Exception

--- src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/exception/ItemNotFoundException.java
+++ src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/exception/ItemNotFoundException.java
@@ -1,5 +1,15 @@
 package lk.ijse.aadassignmentphase02possystembackendusingspringmvc.exception;
 
-public class ItemNotFoundException {
+public class ItemNotFoundException extends RuntimeException{
+    public ItemNotFoundException(){
+
+    }
+
+    public ItemNotFoundException(String message){
+
+    }
+    public ItemNotFoundException(String message,Exception cause){
+
+    }
 
 }
