4fa74b79b1109652b3f07967e5b878825fc4fd35
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-10-13 08:54:19 +0530

    Create CustomerService interface and implement it

--- src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/service/CustomerService.java
+++ src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/service/CustomerService.java
@@ -1,4 +1,14 @@
 package lk.ijse.aadassignmentphase02possystembackendusingspringmvc.service;
 
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dto.CustomerStatus;
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dto.impl.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO customerDTO);
+    List<CustomerDTO> getAllCustomers();
+    CustomerStatus getCustomer(String customerId);
+    void updateCustomer(String customerId,CustomerDTO customerDTO);
+    void deleteCustomer(String customerId);
 }
--- src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,34 @@
 package lk.ijse.aadassignmentphase02possystembackendusingspringmvc.service.impl;
 
-public class CustomerServiceImpl {
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dto.CustomerStatus;
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.dto.impl.CustomerDTO;
+import lk.ijse.aadassignmentphase02possystembackendusingspringmvc.service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+        
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public CustomerStatus getCustomer(String customerId) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(String customerId, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String customerId) {
+
+    }
 }
