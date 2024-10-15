1ba62a959a5aaec43ab63159b423f73a28ad56e9
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-10-15 10:58:40 +0530

    add frontend backend connect configurations

--- src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/config/WebAppConfig.java
+++ src/main/java/lk/ijse/aadassignmentphase02possystembackendusingspringmvc/config/WebAppConfig.java
@@ -3,7 +3,9 @@ package lk.ijse.aadassignmentphase02possystembackendusingspringmvc.config;
 import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //root package eka - lk.ijse.introspringmvc
@@ -14,5 +16,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
         maxFileSize = 1024 * 1024 * 5,
         maxRequestSize = 1024 * 1024 * 10
 )
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all paths
+                .allowedOrigins("http://localhost:63342") // Allow your frontend's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
